7dbe646aedc5e8918308ed5374a4555af884533a
George Studenko <gnstudenko@gmail.com>
2019-08-28 16:17:39 +0200

    Implement Sender

--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using RabbitMQ.Client;
+using System;
 using System.Text;
 
 namespace Producer
 {
     public class Sender
     {
+        public static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+                using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("TestQueue", false, false, false, null);
+
+                string message = "Hello RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "TestQueue", null, body);
+                Console.WriteLine($"Sent message {message}");
+            }
+            Console.WriteLine($"Press any key to exit the sender app");
+            Console.ReadKey();
+        }
+
     }
 }
